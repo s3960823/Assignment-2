48d97f62a9dffe30d339c1b1385fff5184be79eb
s3960823 <s3960823@studey.rmit.edu.au>
2023-09-22 18:05:26 +1000

    Initial Commit with the sample solution provided of Assignment-1

---
+++ ParseValueException.java
@@ -0,0 +1,12 @@
+/**
+ * Custom exception thrown when an invalid input is provided during the parsing
+ * process
+ *
+ * @author AA
+ * @version 1.0.0
+ */
+public class ParseValueException extends Exception {
+    ParseValueException(String msg) {
+        super(msg);
+    }
+}
